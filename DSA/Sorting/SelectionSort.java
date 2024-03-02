6a1cde408b76f31f89fef32749e84d1834471ff6
gitadi <aduvishwakarma123@gmail.com>
2024-03-02 12:33:56 +0530

    selectionsort

--- DSA/Sorting/SelectionSort.java
+++ DSA/Sorting/SelectionSort.java
@@ -4,22 +4,24 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr1 = {4,5,1,2,3};
-        System.out.println("sorted array = " + Arrays.toString(selectionSort(arr1)));
+        int[] arr1 = {-22,0,1,36,-12,2,-2};
+        selectionSort(arr1);
+        System.out.println("sorted array = " + Arrays.toString(arr1));
     }
-    static int[] selectionSort(int[] arr) {
+    static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             // Finding max ele in the remaining array and swapping it with the last ele
-            int last = arr[arr.length - 1 - i];
+            int last = arr.length - 1 - i;
             int max = maxEle(arr, 0, arr.length - 1 - i);
+            swap(arr, max, last);
         }
     }
 
     static int maxEle(int[] arr, int start, int end) {
-        int max = arr[start];
+        int max = start;
         for (int i = start + 1; i <= end; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+            if (arr[i] > arr[max]) {
+                max = i;
             }
         }
         return max;
